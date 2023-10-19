b4a9147147d0a7a17fb00e5429c2e52df066f2c8
Najlaruiz <najla.badraouy@gmail.com>
2023-10-19 15:52:55 +0000

    calculator-native js, html,css

--- main.js
+++ main.js
@@ -26,15 +26,15 @@ function button_number(button) {
     
     last_button = button;
 
-    // if button is not an operator or = sign
+    
     if (!operators.includes(button) && button!=equal){
-        // if it is the first button clicked
+       
         if (firstNum){
-            // and it's a dot, show 0.
+         
             if (button == dot){
                 box.innerText = "0"+dot;
             }
-            // else clear box and show the number
+          
             else {
                 box.innerText = button;
             }
@@ -42,7 +42,7 @@ function button_number(button) {
         }
         else {
 
-            // return if the box value is 0
+            
             if (box.innerText.length == 1 && box.innerText == 0){
 
                 if (button == dot){
@@ -50,34 +50,33 @@ function button_number(button) {
                 }
                 return;
             }
-            // return if the box already has a dot and clicked button is a dot
+            
             if (box.innerText.includes(dot) && button == dot){
                 return;
             }
-            // maximum allowed numbers inputted are 20
             if (box.innerText.length == 20){
                 return;
             }
 
-            // if pressed dot and box already has a - sign, show -0.
+            
             if (button == dot && box.innerText == "-"){
                 box.innerText = "-0"+dot;
             }
-            // else append number
+            
             else {
                 box.innerText += button;
             }  
         }
     }
-    // if it's an operator or = sign
+  
     else {
 
-        // return if operator is already pressed
+     
         if (operator_value != null && button == operator_value){
             return
         }
 
-        // show minus sign if it's the first value selected and finally return
+        
         if (button == "-" && box.innerText == 0){
             box.innerText = button;
             firstNum = false;
@@ -85,16 +84,15 @@ function button_number(button) {
             showSelectedOperator()
             return;
         }
-        // return if minus operator pressed and it's already printed on screen 
+         
         else if (operators.includes(button) && box.innerText == "-"){
             return
         }
-        // return if minus operator pressed and history already has equal sign
+        
         else if (button == "-" && operator_value == "-" && last_operation_history.innerText.includes("=")){
             return
         }
 
-        // set value of operator if it's one
         if (operators.includes(button)){
             if (typeof last_operator != "undefined" && last_operator != null){
                 calc_operator = last_operator
@@ -116,26 +114,26 @@ function button_number(button) {
             showSelectedOperator()
         }
 
-        // add first number to numbers array and show it on history
+        
         if (numbers.length == 0){
             numbers.push(box.innerText)
             if (typeof last_operator != "undefined" && last_operator != null){
                 last_operation_history.innerText = box.innerText + " " + last_operator
             }
         }
-        // rest of calculations
+        
         else {   
             if (numbers.length == 1){
                 numbers[1] = box.innerText
             }
             var temp_num = box.innerText
 
-            // calculate total
+          
             if (button==equal && calc_operator != null){
                 var total = calculate(numbers[0], numbers[1], calc_operator)
                 box.innerText = total;
 
-                // append second number to history
+               
                 if (!last_operation_history.innerText.includes("=")){
                     last_operation_history.innerText += " " + numbers[1] + " ="
                 }
@@ -146,12 +144,12 @@ function button_number(button) {
                 operator_value = null
                 showSelectedOperator()
 
-                // replace first number of history with the value of total
+                
                 var history_arr = last_operation_history.innerText.split(" ")
                 history_arr[0] = temp_num
                 last_operation_history.innerText = history_arr.join(" ")
             }
-            // update history with the value on screen and the pressed operator
+            
             else if (calc_operator != null) {
                  last_operation_history.innerText = temp_num + " " + last_operator
                  calc_operator = button
@@ -162,8 +160,8 @@ function button_number(button) {
     }
 
 }
- // highlight operator button when selected
-function showSelectedOperator(){
+ 
+   function showSelectedOperator(){
 
     var elements = document.getElementsByClassName("operator");
 
@@ -185,8 +183,8 @@ function showSelectedOperator(){
     }
 }
 
-// function to calculate the result using two number and an operator
-function calculate(num1, num2, operator){
+
+    function calculate(num1, num2, operator){
 
     if (operator === "+"){
         total = (parseFloat)(num1)+(parseFloat)(num2)
@@ -208,14 +206,14 @@ function calculate(num1, num2, operator){
             return box.innerText
         }
     }
-    // if total is not integer, show maximum 12 decimal places
+    
     if (!Number.isInteger(total)){
         total = total.toPrecision(12);
     }
     return parseFloat(total);
 }
 
-// function to clear box and reset everything
+
 function button_clear(){
     window.location.reload()
 }
@@ -234,7 +232,6 @@ function backspace_remove(){
     
     box.innerText = last_num
 
-    // show 0 zero if all characters on screen are removed
     if (box.innerText.length == 0){
         box.innerText = 0
         firstNum = true
@@ -243,28 +240,27 @@ function backspace_remove(){
 }
 
 
-// function to change the sign of the number currently on screen
 function plus_minus(){
     box = document.getElementById("box");
 
-    // if any operator is already pressed
+   
     if (typeof last_operator != "undefined"){
         if (numbers.length>0){
-            // if last button pressed is an operator
+            
             if (operators.includes(last_button)){
-                // if the displayed text is just a negative sign, replace it with a 0
+
                 if (box.innerText == "-"){
                     box.innerText = 0
                     firstNum = true
                     return
                 }
-                // if the displayed text is not a just a negative sign, replace it with a negative sign
+                
                 else {
                     box.innerText = "-"
                     firstNum = false
                 }
             }
-            // if last button pressed is not an operator, change its sign
+            
             else {
                 box.innerText = -box.innerText
 
@@ -279,7 +275,7 @@ function plus_minus(){
         return
     }
 
-    // if displayed text is 0, replace it with a negative sign
+    
     if (box.innerText == 0){
         box.innerText = "-"
         firstNum = false
@@ -288,7 +284,7 @@ function plus_minus(){
     box.innerText = -box.innerText
 }
 
-// function to calculate square root of the number currently on screen
+
 function square_root(){
     box = document.getElementById("box");
     var square_num = Math.sqrt(box.innerText)
@@ -296,7 +292,7 @@ function square_root(){
     numbers.push(square_num)
 }
 
-// function to calculate the division of 1 with the number currently on screen
+
 function division_one(){
     box = document.getElementById("box");
     var square_num = 1/box.innerText
@@ -304,7 +300,7 @@ function division_one(){
     numbers.push(square_num)
 }
 
-// function to calculate the power of the number currently on screen
+
 function power_of(){
     box = document.getElementById("box");
     var square_num =Math.pow(box.innerText, 2)
@@ -312,7 +308,7 @@ function power_of(){
     numbers.push(square_num)
 }
 
-// function to calculate the percentage of a number
+
 function calculate_percentage(){
     var elements = document.getElementsByClassName("operator");
     box = document.getElementById("box");
@@ -326,7 +322,7 @@ function calculate_percentage(){
         box.innerText = perc_value
         numbers.push(box.innerText)
     
-        // append second number to history
+      
         if (!last_operation_history.innerText.includes("=")){
             last_operation_history.innerText += " " + numbers[1] + " ="
         }
@@ -340,13 +336,13 @@ function calculate_percentage(){
     box.innerText = res
     operator_value = "="
 
-    // deselect operator if any selected
+  
     for (var i=0; i<elements.length; i++){
         elements[i].style.backgroundColor  = "#e68a00";
     }
 }
 
-// function to clear last number typed into the display
+
 function clear_entry(){
     box = document.getElementById("box");
 
@@ -362,7 +358,6 @@ function clear_entry(){
 document.addEventListener('keydown', keyPressed);
 document.addEventListener('keyup', keyReleased);
 
-// function to capture keydown events
 function keyPressed(e) {
     e.preventDefault()
     var equal = document.getElementById("equal_sign").value;
@@ -409,7 +404,7 @@ function keyPressed(e) {
     }
 }
 
-// function to capture keyup events
+
 function keyReleased(e){
     if (key_combination['ControlLeft'] && key_combination['KeyV']) {
         navigator.clipboard.readText().then(text => {
@@ -430,7 +425,7 @@ function keyReleased(e){
     }
     key_combination = []
     e.preventDefault()
-    // set the color of the backspace button back to its original
+    
     if (e.key == "Backspace"){
         document.getElementById("backspace_btn").style.backgroundColor  = "#666666";
     }
